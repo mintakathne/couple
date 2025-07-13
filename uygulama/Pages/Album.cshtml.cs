6ab51153c1dee041587766c6bef164c931e88cee
esmanur yavuz <yavuzesmanur13@gmail.com>
2025-07-13 20:32:28 +0300

    Initial commit

---
+++ uygulama/Pages/Album.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace uygulama.Pages
+{
+    public class AlbumModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
